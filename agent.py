5d952ddca97f46516729b43bf475385e14843e62
Ivan Husarov <ivan.husarov@gmail.com>
2023-12-08 16:48:44 +0200

    iteration 2 to be tuned

---
+++ agent.py
@@ -0,0 +1,163 @@
+import torch
+import random
+import numpy as np
+from collections import deque
+from battlefield import Direction, BattlefieldAI, Pos
+from model import Linear_QNet, QTrainer
+from functools import reduce
+from helper import plot
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+
+class Agent:
+
+    def __init__(self):
+        self.n_games = 0
+        self.epsilon = 0 # randomness
+        self.gamma = 0.9 # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY) # popleft()
+        self.model = Linear_QNet(12, 256, 4)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
+
+
+    def get_state(self, game):
+        unit = game.friendly_unit
+        point_l = Pos(unit.pos.x - 1, unit.pos.y)
+        point_r = Pos(unit.pos.x + 1, unit.pos.y)
+        point_u = Pos(unit.pos.x, unit.pos.y - 1)
+        point_d = Pos(unit.pos.x, unit.pos.y + 1)
+        
+        dir_l = game.direction == Direction.LEFT
+        dir_r = game.direction == Direction.RIGHT
+        dir_u = game.direction == Direction.UP
+        dir_d = game.direction == Direction.DOWN
+
+        acc_x = [0, 0]
+        for enemy in game.enemies:
+            acc_x = [acc_x[0] + enemy.health, acc_x[1]] if enemy.pos.x < game.friendly_unit.pos.x else [acc_x[0], acc_x[1] + enemy.health]
+
+        acc_y = [0, 0]
+        for enemy in game.enemies:
+            acc_y = [acc_y[0] + enemy.health, acc_y[1]] if enemy.pos.y < game.friendly_unit.pos.y else [acc_y[0], acc_y[1] + enemy.health]
+
+        state = [
+            # Danger straight
+            (dir_r and game.is_collision(point_r)) or 
+            (dir_l and game.is_collision(point_l)) or 
+            (dir_u and game.is_collision(point_u)) or 
+            (dir_d and game.is_collision(point_d)),
+
+            # Danger bottom
+            (dir_l and game.is_collision(point_r)) or 
+            (dir_r and game.is_collision(point_l)) or 
+            (dir_d and game.is_collision(point_u)) or 
+            (dir_u and game.is_collision(point_d)),
+
+            # Danger right
+            (dir_u and game.is_collision(point_r)) or 
+            (dir_d and game.is_collision(point_l)) or 
+            (dir_l and game.is_collision(point_u)) or 
+            (dir_r and game.is_collision(point_d)),
+
+            # Danger left
+            (dir_d and game.is_collision(point_r)) or 
+            (dir_u and game.is_collision(point_l)) or 
+            (dir_r and game.is_collision(point_u)) or 
+            (dir_l and game.is_collision(point_d)),
+
+            
+            # Move direction
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d, 
+
+            # Enemy health location fraction
+            acc_x[0] / max(np.sum(acc_x), 1),
+            acc_x[1] / max(np.sum(acc_x), 1),
+            acc_y[0] / max(np.sum(acc_y), 1),
+            acc_y[1] / max(np.sum(acc_y), 1),
+           ]
+
+        return np.array(state, dtype=float)
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done)) # popleft if MAX_MEMORY is reached
+
+    def train_long_memory(self):
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE) # list of tuples
+        else:
+            mini_sample = self.memory
+
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
+        #for state, action, reward, nexrt_state, done in mini_sample:
+        #    self.trainer.train_step(state, action, reward, next_state, done)
+
+    def train_short_memory(self, state, action, reward, next_state, done):
+        self.trainer.train_step(state, action, reward, next_state, done)
+
+    def get_action(self, state):
+        # random moves: tradeoff exploration / exploitation
+        self.epsilon = 80 - self.n_games
+        final_move = [0,0,0,0]
+        if random.randint(0, 200) < self.epsilon:
+            move = random.randint(0, 2)
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+
+        return final_move
+
+
+def train():
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = BattlefieldAI()
+    while True:
+        # get old state
+        state_old = agent.get_state(game)
+
+        # get move
+        final_move = agent.get_action(state_old)
+
+        # perform move and get new state
+        reward, done, score = game.play_step(final_move)
+        state_new = agent.get_state(game)
+
+        # train short memory
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
+
+        # remember
+        agent.remember(state_old, final_move, reward, state_new, done)
+
+        if done:
+            # train long memory, plot result
+            game.reset()
+            agent.n_games += 1
+            agent.train_long_memory()
+
+            if score > record:
+                record = score
+                agent.model.save()
+
+            print('\nGame', agent.n_games, 'Score', score, 'Record:', record)
+
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
+
+
+if __name__ == '__main__':
+    train()
---
+++ battlefield.py
@@ -0,0 +1,247 @@
+import pygame
+import numpy as np
+import math
+import random
+from enum import Enum
+from collections import namedtuple
+
+# font = pygame.font.SysFont('arial', 25)
+
+class Pos:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+  def __str__(self):
+    return f"x = {self.x}, y is {self.y}"
+    
+class Enemy:
+  def __init__(self, threat, pos):
+    self.health = 10 * threat
+    self.damage = 10 * threat
+    self.threat = threat
+    self.pos = pos
+  def __str__(self):
+    return f"health = {self.health}, pos = {self.pos}"
+
+class Unit:
+  def __init__(self, health, damage, pos):
+    self.health = health
+    self.damage = damage
+    self.pos = pos
+
+def generate_data(n):
+    battlemap = np.array([np.random.uniform(low=0, high=1 - i / n, size=(n,)) for i in range(n)])
+    enemies = generate_enemies(n)
+
+    return (battlemap, enemies)
+
+def generate_enemies(n):
+    enemymap = np.array([np.random.permutation([1 * np.random.uniform(low=0.4, high=1)] * math.ceil(1) + [0] * math.floor(n - 1)) for i in range(math.ceil(n / 2))] + [[0] * n for i in range(math.floor(n / 2))])
+    enemies = np.array([Enemy(enemymap[x][y], Pos(x, y)) for [x, y] in np.transpose((enemymap > 0).nonzero())]);
+    return enemies
+
+
+window = pygame.display.set_mode((510,510))
+active = True
+n = 20
+scale = 500 / n
+
+WHITE = (255, 255, 255)
+RED = (200,0,0)
+BLUE1 = (0, 0, 255)
+BLUE2 = (0, 100, 255)
+BLACK = (0,0,0)
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+
+# friendly_unit = Unit(10, 10, Pos(50, 50))
+
+# battlemap, enemies = generate_data(n) 
+
+# clock = pygame.time.Clock()
+
+# while active:
+#    window.fill((0, 0, 0))
+#    for event in pygame.event.get():
+#       if event.type == pygame.QUIT:
+#          active = False
+
+#    pygame.draw.rect(window, (0,0,255), pygame.Rect(friendly_unit.pos.y * scale, friendly_unit.pos.x * scale, scale, scale))
+
+#    for enemy in enemies[:1]:
+#         pygame.draw.rect(window, (255, 1 / (255 * enemy.threat), 255 * enemy.threat), pygame.Rect(enemy.pos.y * scale, enemy.pos.x * scale, scale, scale))
+# #    pygame.draw.circle(window,red,(circleX,circleY),radius) # DRAW CIRCLE
+
+#    pygame.display.update()
+#    pygame.display.flip()
+#    clock.tick(10)
+# #    friendly_unit.pos.x -= 1
+#    friendly_unit.pos.y -= 1
+
+
+class BattlefieldAI:
+
+    def __init__(self,  w=500, h=500):
+        self.w = w
+        self.h = h
+
+        battlemap, enemies = generate_data(n) 
+        self.battlemap = battlemap
+        # self.friendly_unit = Unit(100, 10, Pos(40, 40))
+        # init display
+        self.display = pygame.display.set_mode((self.w, self.h))
+        pygame.display.set_caption('Battlefield')
+        self.score = 0
+        self.clock = pygame.time.Clock()
+        self.reset()
+
+    def reset(self):
+        self.friendly_unit = Unit(100, 10, Pos(10, 10))
+        self.frame_iteration = 0
+        self.score = 0
+        self.enemies = generate_enemies(n)
+
+        self.direction = Direction.UP
+        
+    # def play_step(self):
+    def play_step(self, action):
+        self.frame_iteration += 1
+        # 1. collect user input
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            # if event.type == pygame.KEYDOWN:
+            #     if event.key == pygame.K_LEFT:
+            #         self.direction = Direction.LEFT
+            #     elif event.key == pygame.K_RIGHT:
+            #         self.direction = Direction.RIGHT
+            #     elif event.key == pygame.K_UP:
+            #         self.direction = Direction.UP
+            #     elif event.key == pygame.K_DOWN:
+            #         self.direction = Direction.DOWN
+        # 2. move
+        self._move(action) # update the head
+        # self._move_dir(self.direction)
+
+        if self.is_collision():
+            game_over = True
+            reward = -10
+            return reward, game_over, self.score
+
+        pos_decay = self.battlemap[self.friendly_unit.pos.x][self.friendly_unit.pos.y]
+        self.friendly_unit.health -= pos_decay
+        
+        # 3. check if game over
+        reward = 0
+        game_over = False
+        if self.friendly_unit.health < 0 or self.frame_iteration > 1000:
+            game_over = True
+            reward = -1
+            return reward, game_over, self.score
+
+        # 4. check enemy or just move
+        index, result = next(
+            ((index, enemy) for (index, enemy) in enumerate(self.enemies) if enemy.pos.x == self.friendly_unit.pos.x and enemy.pos.y == self.friendly_unit.pos.y and enemy.health > 0),
+            (None, None)
+        )
+
+        if result is not None:
+            result.health -= self.friendly_unit.damage
+            self.enemies[index] = result
+            self.friendly_unit.health -= result.damage
+            self.score += 1
+            reward = 30 * result.threat
+        else:
+            reward = -pos_decay
+            # reward = 0
+
+        # 5. update ui and clock
+
+        # temp
+        self._update_ui()
+        self.clock.tick(100)
+
+        # 6. return game over and score
+        print("Health: ", self.friendly_unit.health, " Score: ", self.score, end = "\r")
+        return reward, game_over, self.score
+
+
+    def is_collision(self, pt=None):
+        if pt is None:
+            pt = self.friendly_unit.pos
+        # hits boundary
+        if pt.x * scale >= self.w or pt.x < 0 or pt.y * scale >= self.h or pt.y < 0:
+            return True
+
+        return False
+
+
+    def _update_ui(self):
+        self.display.fill(BLACK)
+
+
+        for enemy in self.enemies:
+            if enemy.health > 0: pygame.draw.rect(window, (255, 1 / (255 * enemy.threat), 255 * enemy.threat), pygame.Rect(enemy.pos.x * scale, enemy.pos.y * scale, scale, scale))
+
+        pygame.draw.rect(window, (0,0,255), pygame.Rect(self.friendly_unit.pos.x * scale, self.friendly_unit.pos.y * scale, scale, scale))
+
+        # text = font.render("Health: " + str(self.friendly_unit.health), True, WHITE)
+        # self.display.blit(text, [0, 0])
+
+        pygame.display.flip()
+
+    def _move_dir(self, direction):
+        self.direction = direction
+
+        x = self.friendly_unit.pos.x
+        y = self.friendly_unit.pos.y
+        if self.direction == Direction.RIGHT:
+            x += 1
+        elif self.direction == Direction.LEFT:
+            x -= 1
+        elif self.direction == Direction.DOWN:
+            y += 1
+        elif self.direction == Direction.UP:
+            y -= 1
+
+        self.friendly_unit.pos = Pos(x, y)
+
+    def _move(self, action):
+        # [straight, right, left, back]
+
+        clock_wise = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
+        idx = clock_wise.index(self.direction)
+
+        if np.array_equal(action, [1, 0, 0, 0]):
+            new_dir = clock_wise[idx] # no change
+        elif np.array_equal(action, [0, 1, 0, 0]):
+            next_idx = (idx + 1) % 4
+            new_dir = clock_wise[next_idx] # right turn r -> d -> l -> u
+        elif np.array_equal(action, [0, 0, 1, 0]):
+            next_idx = (idx - 1) % 4
+            new_dir = clock_wise[next_idx] # left turn r -> u -> l -> d
+        else: 
+            next_idx = (idx + 2) % 4
+            new_dir = clock_wise[next_idx] 
+
+        self._move_dir(new_dir)
+
+# if __name__ == '__main__':
+#     game = BattlefieldAI()
+    
+#     # game loop
+#     while True:
+#         reward, game_over, score = game.play_step()
+        
+#         if game_over == True:
+#             break
+        
+#     print('Final Score', score)
+        
+        
+#     pygame.quit()
